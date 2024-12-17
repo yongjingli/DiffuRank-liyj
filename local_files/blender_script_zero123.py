509c491d7f434975ab27745537727483e1dcc470
uu <uu>
2024-12-17 16:04:42 +0800

    add zero123 render

--- local_files/blender_script_zero123.py
+++ local_files/blender_script_zero123.py
@@ -32,6 +32,7 @@ import numpy as np
 
 import bpy
 from mathutils import Vector
+from debug_others import debug_rot_matrix
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -62,18 +63,21 @@ cam.location = (0, 1.2, 0)
 cam.data.lens = 35
 cam.data.sensor_width = 32
 
-cam_constraint = cam.constraints.new(type="TRACK_TO")
-cam_constraint.track_axis = "TRACK_NEGATIVE_Z"
-cam_constraint.up_axis = "UP_Y"
+cam_constraint = cam.constraints.new(type="TRACK_TO") # 创建了一个新的约束，类型为 TRACK_TO，这意味着相机将会朝向指定的目标对象
+cam_constraint.track_axis = "TRACK_NEGATIVE_Z"        # TRACK_NEGATIVE_Z 表示相机的负 Z 轴将朝向目标对象
+cam_constraint.up_axis = "UP_Y"                       # 意味着相机的 Y 轴将始终保持朝上方向
 
 # setup lighting
-bpy.ops.object.light_add(type="AREA")
-light2 = bpy.data.lights["Area"]
+bpy.ops.object.light_add(type="AREA")                 # 添加一个类型为区域光源的光源对象
+light2 = bpy.data.lights["Area"]                      # 获取名为 "Area" 的光源数据。请注意，光源的名称是自动生成的，通常为 "Area"
 light2.energy = 3000
 bpy.data.objects["Area"].location[2] = 0.5
 bpy.data.objects["Area"].scale[0] = 100
 bpy.data.objects["Area"].scale[1] = 100
 bpy.data.objects["Area"].scale[2] = 100
+
+# light_names = [obj.name for obj in scene.objects if obj.type =="LIGHT"]  # 获取所有光源名称
+
 
 render.engine = args.engine
 render.image_settings.file_format = "PNG"
@@ -83,20 +87,25 @@ render.resolution_y = 512
 render.resolution_percentage = 100
 
 scene.cycles.device = "GPU"
-scene.cycles.samples = 128
-scene.cycles.diffuse_bounces = 1
-scene.cycles.glossy_bounces = 1
-scene.cycles.transparent_max_bounces = 3
-scene.cycles.transmission_bounces = 3
-scene.cycles.filter_width = 0.01
-scene.cycles.use_denoising = True
-scene.render.film_transparent = True
+scene.cycles.samples = 128                # 设置每帧渲染的样本数为 128, 样本数越高，图像质量通常越好，但渲染时间也会增加
+scene.cycles.diffuse_bounces = 1          # 设置漫反射光线反弹的最大次数为 1, 较低的反弹次数可以减少渲染时间，但可能会影响场景的真实感
+scene.cycles.glossy_bounces = 1           # 设置镜面反射光线反弹的最大次数为 1
+scene.cycles.transparent_max_bounces = 3  # 设置透明光线的最大反弹次数为 3, 这对于处理透明物体（如玻璃）时的光线传播很重要
+scene.cycles.transmission_bounces = 3     # 设置透射光线的反弹次数为 3，适用于透明材料
+scene.cycles.filter_width = 0.01          # 设置过滤宽度为 0.01，影响光线采样的模糊程度
+scene.cycles.use_denoising = True         # 启用去噪功能，可以在渲染后减少图像中的噪点
+scene.render.film_transparent = True      # 将渲染背景设置为透明，这在合成时非常有用
+
+UNIFORM_LIGHT_DIRECTION = [0.09387503, -0.63953443, -0.7630093]
+BASIC_AMBIENT_COLOR = 0.3                # 设置基础环境光颜色（BASIC_AMBIENT_COLOR）
+BASIC_DIFFUSE_COLOR = 0.7
 
 bpy.context.preferences.addons["cycles"].preferences.get_devices()
 # Set the device_type
 bpy.context.preferences.addons[
     "cycles"
-].preferences.compute_device_type = "CUDA" # or "OPENCL"
+].preferences.compute_device_type = "CUDA"  # or "OPENCL"
+
 
 def sample_point_on_sphere(radius: float) -> Tuple[float, float, float]:
     theta = random.random() * 2 * math.pi
@@ -136,6 +145,7 @@ def randomize_camera():
 
 def set_camera_location(elevation, azimuth, distance):
     # from https://blender.stackexchange.com/questions/18530/
+    # 用于在 Blender 中随机生成相机的位置，并根据该位置计算相机的旋转，使相机朝向原点（0, 0, 0）
     x, y, z = sample_spherical(radius_min=1.5, radius_max=2.2, maxz=2.2, minz=-2.2)
     camera = bpy.data.objects["Camera"]
     camera.location = x, y, z
@@ -235,6 +245,7 @@ def get_3x4_RT_matrix_from_blender(cam):
     #
     # Use matrix_world instead to account for all constraints
     location, rotation = cam.matrix_world.decompose()[0:2]
+
     R_world2bcam = rotation.to_matrix().transposed()
 
     # Convert camera location to translation vector used in coordinate changes
@@ -268,6 +279,214 @@ def normalize_scene():
     bpy.ops.object.select_all(action="DESELECT")
 
 
+def setup_nodes(output_path, MAX_DEPTH=5.0, capturing_material_alpha: bool = False, basic_lighting: bool = False):
+    # 一个用于设置 Blender 的节点系统的函数 setup_nodes，该函数在合成器中创建节点以处理图像和深度信息
+    # output_path: 输出文件的基本路径，用于保存生成的图像文件。
+    # MAX_DEPTH: 最大深度值，用于深度图的归一化。
+    # capturing_material_alpha: 布尔值，指示是否捕获材质的 alpha 通道。
+    # basic_lighting: 布尔值，指示是否启用基础光照计算。
+
+    tree = bpy.context.scene.node_tree
+    links = tree.links
+
+    # 这段代码删除当前节点树中的所有节点，确保每次调用该函数时都是从一个干净的状态开始
+    for node in tree.nodes:
+        tree.nodes.remove(node)
+
+    # Helpers to perform math on links and constants.
+    def node_op(op: str, *args, clamp=False):
+        node = tree.nodes.new(type="CompositorNodeMath")
+        node.operation = op
+        if clamp:
+            node.use_clamp = True
+        for i, arg in enumerate(args):
+            if isinstance(arg, (int, float)):
+                node.inputs[i].default_value = arg
+            else:
+                links.new(arg, node.inputs[i])
+        return node.outputs[0]
+
+    def node_clamp(x, maximum=1.0):
+        return node_op("MINIMUM", x, maximum)
+
+    def node_mul(x, y, **kwargs):
+        return node_op("MULTIPLY", x, y, **kwargs)
+
+    def node_add(x, y, **kwargs):
+        return node_op("ADD", x, y, **kwargs)
+
+    def node_abs(x, **kwargs):
+        return node_op("ABSOLUTE", x, **kwargs)
+
+    # 创建一个输入节点，用于接收场景的渲染层。
+    input_node = tree.nodes.new(type="CompositorNodeRLayers")
+    input_node.scene = bpy.context.scene
+    # CompositorNodeRLayers 是 Blender 中合成节点系统中的一个节点，用于访问场景的渲染层信息。它提供了对渲染结果的各种输出，包括颜色、深度、法线等数据。这个节点是合成工作流程中的重要组成部分，特别是在后期制作和特效处理中。
+    input_sockets = {}
+    for output in input_node.outputs:
+        input_sockets[output.name] = output
+
+    if capturing_material_alpha:
+        color_socket = input_sockets["Image"]
+    else:
+        raw_color_socket = input_sockets["Image"]
+        # 在 Blender 的合成节点系统中计算基础光照的部分
+        if basic_lighting:
+            # Compute diffuse lighting
+            normal_xyz = tree.nodes.new(type="CompositorNodeSeparateXYZ")
+            tree.links.new(input_sockets["Normal"], normal_xyz.inputs[0])
+            normal_x, normal_y, normal_z = [normal_xyz.outputs[i] for i in range(3)]
+            dot = node_add(
+                node_mul(UNIFORM_LIGHT_DIRECTION[0], normal_x),
+                node_add(
+                    node_mul(UNIFORM_LIGHT_DIRECTION[1], normal_y),
+                    node_mul(UNIFORM_LIGHT_DIRECTION[2], normal_z),
+                ),
+            )
+            diffuse = node_abs(dot)
+            # Compute ambient + diffuse lighting
+            brightness = node_add(BASIC_AMBIENT_COLOR, node_mul(BASIC_DIFFUSE_COLOR, diffuse))
+            # Modulate the RGB channels using the total brightness.
+            rgba_node = tree.nodes.new(type="CompositorNodeSepRGBA")
+            tree.links.new(raw_color_socket, rgba_node.inputs[0])
+            combine_node = tree.nodes.new(type="CompositorNodeCombRGBA")
+            for i in range(3):
+                tree.links.new(node_mul(rgba_node.outputs[i], brightness), combine_node.inputs[i])
+            tree.links.new(rgba_node.outputs[3], combine_node.inputs[3])
+            raw_color_socket = combine_node.outputs[0]
+
+        # We apply sRGB here so that our fixed-point depth map and material
+        # alpha values are not sRGB, and so that we perform ambient+diffuse
+        # lighting in linear RGB space.
+        color_node = tree.nodes.new(type="CompositorNodeConvertColorSpace")
+        # color_node.from_color_space = "Linear"
+        color_node.to_color_space = "sRGB"
+        tree.links.new(raw_color_socket, color_node.inputs[0])
+        color_socket = color_node.outputs[0]
+
+    split_node = tree.nodes.new(type="CompositorNodeSepRGBA")
+    tree.links.new(color_socket, split_node.inputs[0])
+    # Create separate file output nodes for every channel we care about.
+    # The process calling this script must decide how to recombine these
+    # channels, possibly into a single image.
+    for i, channel in enumerate("rgba") if not capturing_material_alpha else [(0, "MatAlpha")]:
+        output_node = tree.nodes.new(type="CompositorNodeOutputFile")
+        output_node.base_path = f"{output_path}_{channel}"
+        links.new(split_node.outputs[i], output_node.inputs[0])
+
+    if capturing_material_alpha:
+        # No need to re-write depth here.
+        return
+
+    depth_out = node_clamp(node_mul(input_sockets["Depth"], 1 / MAX_DEPTH))    # 创建深度估计输出的节点
+    output_node = tree.nodes.new(type="CompositorNodeOutputFile")
+    output_node.base_path = f"{output_path}_depth"
+    links.new(depth_out, output_node.inputs[0])
+
+
+def render_scene(render_path):
+    # render the image
+    scene.render.filepath = render_path
+
+    # output depth
+    use_workbench = True
+
+    # depth
+    bpy.context.scene.render.engine == "CYCLES"
+    bpy.context.scene.cycles.samples = 16
+    bpy.context.scene.cycles.use_denoising = True
+    bpy.context.scene.cycles.denoiser = 'OPTIX'
+
+    bpy.context.view_layer.update()
+    bpy.context.scene.use_nodes = True
+    bpy.context.scene.view_layers["ViewLayer"].use_pass_z = True
+
+    bpy.context.scene.view_settings.view_transform = "Raw"  # sRGB done in graph nodes
+    bpy.context.scene.render.film_transparent = True
+    bpy.context.scene.render.resolution_x = 512
+    bpy.context.scene.render.resolution_y = 512
+    bpy.context.scene.render.image_settings.file_format = "PNG"
+    bpy.context.scene.render.image_settings.color_mode = "BW"   # 这行代码将图像的颜色模式设置为黑白（BW）。这意味着渲染将输出灰度图像，所有颜色信息将被转换为亮度值。
+    bpy.context.scene.render.image_settings.color_depth = "16"  # 将图像的颜色深度设置为 16 位。这意味着每个颜色通道将有 65536 个可能的值
+    bpy.context.scene.render.filepath = render_path
+
+    setup_nodes(render_path)
+    bpy.ops.render.render(write_still=True)
+
+    # bpy.ops.render.render(write_still=True)
+
+    for channel_name in ["r", "g", "b", "a", "depth"]:
+        sub_dir = f"{render_path}_{channel_name}"
+        image_path = os.path.join(sub_dir, os.listdir(sub_dir)[0])
+        name, ext = os.path.splitext(render_path)
+        if channel_name == "depth" or not use_workbench:
+            os.rename(image_path, f"{name}_{channel_name}{ext}")
+        else:
+            os.remove(image_path)
+        os.removedirs(sub_dir)
+
+    # RGB
+    if use_workbench:
+        # bpy.context.scene.use_nodes 是一个布尔属性，用于控制当前场景是否使用合成节点。如果将其设置为 False，则表示禁用节点系统。
+        # 在这种情况下，Blender 将不再使用合成节点来处理渲染结果，所有的图像处理将依赖于传统的渲染设置。
+
+        bpy.context.scene.use_nodes = False
+        bpy.context.scene.render.engine == "CYCLES"
+        bpy.context.scene.cycles.samples = 16
+        bpy.context.scene.cycles.use_denoising = True
+        bpy.context.scene.cycles.denoiser = 'OPTIX'
+        bpy.context.scene.render.image_settings.color_mode = "RGBA"
+        bpy.context.scene.render.image_settings.color_depth = "16"
+        os.remove(render_path)
+        bpy.ops.render.render(write_still=True)
+
+
+def scene_fov():
+    x_fov = bpy.context.scene.camera.data.angle_x
+    y_fov = bpy.context.scene.camera.data.angle_y
+    width = bpy.context.scene.render.resolution_x
+    height = bpy.context.scene.render.resolution_y
+    if bpy.context.scene.camera.data.angle == x_fov:
+        y_fov = 2 * math.atan(math.tan(x_fov / 2) * height / width)
+    else:
+        x_fov = 2 * math.atan(math.tan(y_fov / 2) * width / height)
+    return x_fov, y_fov
+
+
+def write_camera_metadata(camera, path):
+    x_fov, y_fov = scene_fov()
+    bbox_min, bbox_max = scene_bbox()
+    # matrix = bpy.context.scene.camera.matrix_world
+    matrix = camera.matrix_world
+    _, _, scale = camera.matrix_world.decompose()[0:3]
+    with open(path, "w") as f:
+        json.dump(
+            dict(
+                format_version=6,
+                max_depth=5.0,
+                bbox=[list(bbox_min), list(bbox_max)],
+                origin=list(matrix.col[3])[:3],
+                x_fov=x_fov,
+                y_fov=y_fov,
+                x=[t/scale.x for t in list(matrix.col[0])[:3]],
+                y=[t/scale.y for t in list(-matrix.col[1])[:3]],
+                z=[t/scale.z for t in list(-matrix.col[2])[:3]],
+            ),
+            f,
+        )
+
+def rotation_matrix_to_quaternion(R):
+    # 确保输入是一个 3x3 矩阵
+    assert R.shape == (3, 3)
+
+    # 计算四元数的分量
+    w = np.sqrt(1.0 + R[0, 0] + R[1, 1] + R[2, 2]) / 2.0
+    x = (R[2, 1] - R[1, 2]) / (4.0 * w)
+    y = (R[0, 2] - R[2, 0]) / (4.0 * w)
+    z = (R[1, 0] - R[0, 1]) / (4.0 * w)
+
+    return np.array([w, x, y, z])
+
 def save_images(object_file: str) -> None:
     """Saves rendered images of the object in the scene."""
     os.makedirs(args.output_dir, exist_ok=True)
@@ -280,6 +499,7 @@ def save_images(object_file: str) -> None:
     normalize_scene()
 
     # create an empty object to track
+    # 将之前创建的相机约束的目标设置为刚刚创建的空对象。这样，相机将会跟踪这个空对象的位置, 可以在 3D 视图中看到并操作这个空对象
     empty = bpy.data.objects.new("Empty", None)
     scene.collection.objects.link(empty)
     cam_constraint.target = empty
@@ -302,13 +522,17 @@ def save_images(object_file: str) -> None:
 
         # render the image
         render_path = os.path.join(args.output_dir, object_uid, f"{i:03d}.png")
-        scene.render.filepath = render_path
-        bpy.ops.render.render(write_still=True)
+        render_scene(render_path)
+
+        # scene.render.filepath = render_path
+        # bpy.ops.render.render(write_still=True)
 
         # save camera RT matrix
         RT = get_3x4_RT_matrix_from_blender(camera)
         RT_path = os.path.join(args.output_dir, object_uid, f"{i:03d}.npy")
         np.save(RT_path, RT)
+
+        write_camera_metadata(camera, os.path.join(args.output_dir, object_uid, f"{i:03d}.json"))
 
 
 def download_object(object_url: str) -> str:
--- local_files/debug_others.py
+++ local_files/debug_others.py
@@ -1,3 +1,5 @@
+import copy
+from mathutils import Vector, Matrix
 import cv2
 # import matplotlib.pyplot as plt
 from PIL import Image
@@ -7,9 +9,23 @@ from mathutils import Vector
 from mathutils.noise import random_unit_vector
 import pickle
 import math
+import numpy as np
+import mathutils
+import os
+import random
+UNIFORM_LIGHT_DIRECTION = [0.09387503, -0.63953443, -0.7630093]
+
+bpy.ops.object.light_add(type="AREA")
+light2 = bpy.data.lights["Area"]
+light2.energy = 3000
+bpy.data.objects["Area"].location[2] = 0.5
+bpy.data.objects["Area"].scale[0] = 100
+bpy.data.objects["Area"].scale[1] = 100
+bpy.data.objects["Area"].scale[2] = 100
 
 
-def read_img():
+def read_render_img():
+    # 读取渲染出来的数据，RGBA读取，以及读取的背景颜色
     # 生成的数据
     img_path0 = "/home/pxn-lyj/Egolee/programs/DiffuRank-liyj/example_material/Cap3D_imgs/9bc465a181f349908b95d030f541663a/00000.png"
     # cap3d的数据
@@ -18,7 +34,6 @@ def read_img():
     img_path2 = "/home/pxn-lyj/Egolee/data/objaverse_data/views_release/0a0c6d3b5f58499db8d6d649ba8de189/000.png"
     # sharpnet的数据
     img_path3 = "/home/pxn-lyj/Egolee/data/shapenet_srn_data/srn_cars/cars_train/1a1dcd236a1e6133860800e6696b8284/rgb/000001.png"
-
 
     # img0 = cv2.imread(img_path0)   # 这种方式读取背景是黑的 BGR
     # img1 = cv2.imread(img_path1)
@@ -49,10 +64,13 @@ def read_img():
     #
     # plt.show()
 
+
 def debug_load_obj():
+    # 读取obj文件，不同的版本的blender采用读取的接口不一样
     path = "/home/pxn-lyj/Egolee/data/meshs/obj_000490/taizi1kgxiyiye.obj"
     # bpy.ops.import_scene.obj(filepath=path)
     bpy.ops.wm.obj_import(filepath=path)
+
 
 def clear_lights():
     bpy.ops.object.select_all(action="DESELECT")
@@ -60,6 +78,7 @@ def clear_lights():
         if isinstance(obj.data, bpy.types.Light):
             obj.select_set(True)
     bpy.ops.object.delete()
+
 
 def create_light(location, energy=1.0, angle=0.5 * math.pi / 180):
     # https://blender.stackexchange.com/questions/215624/how-to-create-a-light-with-the-python-api-in-blender-2-92
@@ -78,6 +97,7 @@ def create_light(location, energy=1.0, angle=0.5 * math.pi / 180):
 
     
 def create_uniform_light(backend):
+    # 创建光源，可以在blender中查看具体光源的位置
     clear_lights()
     # Random direction to decorrelate axis-aligned sides.
     pos = Vector(UNIFORM_LIGHT_DIRECTION)
@@ -90,12 +110,318 @@ def debug_create_uniform_light():
     create_uniform_light(backend="BLENDER_EEVEE")
 
 
+def load_depth():
+    # 读取渲染出来的深度,
+    # depth_path = "/home/pxn-lyj/Egolee/programs/DiffuRank-liyj/example_material/Cap3D_imgs/9bc465a181f349908b95d030f541663a/00001_depth.png"
+    depth_path = "/home/pxn-lyj/Egolee/programs/DiffuRank-liyj/local_files/example_material/taizi1kgxiyiye/000_depth.png"
+    depth = Image.open(depth_path)
+    depth = np.array(depth)
+    print(depth.shape)
+
+    # 获取深度的方式如下
+    max_store_num = np.iinfo(depth.dtype).max
+    max_depth = 5.0
+    cap3d_depth = depth / max_store_num * max_depth
+
+
+def debug_camera_matrix_world_rot_matrix():
+    # 对比bpy.context.scene.camera.matrix_world.decompose() 与bpy.context.scene.camera.matrix_world的区别
+    # 最大的区别在于bpy.context.scene.camera.matrix_world的旋转矩阵包含了scale在里面，实际上相机位姿不需要包含scale，如何scale不为1时，需要对矩阵进行进一步的/scale
+    import bpy
+    # rotation_matrix_1_scale 与 rotation_matrix_2等价
+    # 方法 1
+    # camera = bpy.data.objects['Camera']  # 替换为你的摄像机名称
+    # location, rotation, scale = camera.matrix_world.decompose()[0:3]
+    location, rotation, scale = bpy.context.scene.camera.matrix_world.decompose()[0:3]
+    rotation_matrix_1 = rotation.to_matrix()
+
+    # 设置旋转+scale
+    rotation_matrix_1_scale = copy.deepcopy(rotation_matrix_1)
+    rotation_matrix_1_scale.col[0] *= scale.x
+    rotation_matrix_1_scale.col[1] *= scale.y
+    rotation_matrix_1_scale.col[2] *= scale.z
+
+    # 方法 2
+    matrix = bpy.context.scene.camera.matrix_world
+    rotation_matrix_2 = matrix.to_3x3()
+
+    # 比较两个旋转矩阵
+    are_equal = (rotation_matrix_1 == rotation_matrix_2)
+
+    m_scale_0 = (rotation_matrix_1.col[0][0] + 1e-6)/(rotation_matrix_2.col[0][0] + 1e-6)
+    m_scale_1 = (rotation_matrix_1.col[0][1] + 1e-6)/(rotation_matrix_2.col[0][1] + 1e-6)
+    m_scale_2 = (rotation_matrix_1.col[0][2] + 1e-6)/(rotation_matrix_2.col[0][2] + 1e-6)
+    m_scale_3 = (rotation_matrix_1.col[1][0] + 1e-6)/(rotation_matrix_2.col[1][0] + 1e-6)
+
+    # 打印结果
+    print("scale:", scale)
+    print("m_scale:", m_scale_0, m_scale_1, m_scale_2, m_scale_3)
+    print("Rotation Matrix 1:", rotation_matrix_1)
+    print("Rotation Matrix 1 scale:", rotation_matrix_1_scale)
+    print("Rotation Matrix 2:", rotation_matrix_2)
+    print("Are the rotation matrices equal?", are_equal)
+    exit(1)
+
+
+def scene_root_objects():
+    for obj in bpy.context.scene.objects.values():
+        if not obj.parent:
+            yield obj
+
+
+def debug_camera_matrix_world_scale():
+    # camera.matrix_world.decompose()是不包含scale的
+    # 对于cap3d，为scale后创建了相机，此时的相机scale为1,不受前面的影响，所有保存旋转矩阵时
+    # 采用bpy.context.scene.camera.matrix_world.col的方式保存矩阵也是正确的，此时的scale为1
+
+    location, rotation, scale = bpy.context.scene.camera.matrix_world.decompose()[0:3]
+    print(location, rotation, scale)
+
+    scale = 0.6
+    for obj in scene_root_objects():
+        obj.scale = obj.scale * scale
+
+    # Apply scale to matrix_world.
+    bpy.context.view_layer.update()
+    location, rotation, scale = bpy.context.scene.camera.matrix_world.decompose()[0:3]
+    print(location, rotation, scale)
+    # 相机的位置是不需要考虑scale的, 如果是通过matrix = bpy.context.scene.camera.matrix_world得到位姿，需要注意当前的scale是否为1
+    # 如果不为1,需要对旋转矩阵进行scale的除
+
+    camera_data = bpy.data.cameras.new(name="Camera")
+    camera_object = bpy.data.objects.new("Camera", camera_data)
+    bpy.context.scene.collection.objects.link(camera_object)
+    bpy.context.scene.camera = camera_object
+    location, rotation, scale = bpy.context.scene.camera.matrix_world.decompose()[0:3]
+    print(location, rotation, scale)
+    # 在zero123中是在scale(normalize_scene)前就创建了相机，scale为整体的scale尺寸
+    # 在render_script_type2(cap3d)中,是在normalize_scene后，创建相机create_camera,采用bpy.context.scene.camera.matrix_world获取相机位姿时scale为1
+
+def reset_scene() -> None:
+    """Resets the scene to a clean state."""
+    # delete everything that isn't part of a camera or a light
+    for obj in bpy.data.objects:
+        if obj.type not in {"CAMERA", "LIGHT"}:
+            bpy.data.objects.remove(obj, do_unlink=True)
+    # delete all the materials
+    for material in bpy.data.materials:
+        bpy.data.materials.remove(material, do_unlink=True)
+    # delete all the textures
+    for texture in bpy.data.textures:
+        bpy.data.textures.remove(texture, do_unlink=True)
+    # delete all the images
+    for image in bpy.data.images:
+        bpy.data.images.remove(image, do_unlink=True)
+
+
+def load_object(object_path: str) -> None:
+    """Loads a glb model into the scene."""
+    if object_path.endswith(".glb"):
+        bpy.ops.import_scene.gltf(filepath=object_path, merge_vertices=True)
+    elif object_path.endswith(".fbx"):
+        bpy.ops.import_scene.fbx(filepath=object_path)
+    elif object_path.endswith(".obj"):
+        try:
+            bpy.ops.import_scene.obj(filepath=object_path)
+        except:
+            bpy.ops.wm.obj_import(filepath=object_path)
+    else:
+        raise ValueError(f"Unsupported file type: {object_path}")
+
+def normalize_scene():
+    bbox_min, bbox_max = scene_bbox()
+    scale = 1 / max(bbox_max - bbox_min)
+    for obj in scene_root_objects():
+        obj.scale = obj.scale * scale
+    # Apply scale to matrix_world.
+    bpy.context.view_layer.update()
+    bbox_min, bbox_max = scene_bbox()
+    offset = -(bbox_min + bbox_max) / 2
+    for obj in scene_root_objects():
+        obj.matrix_world.translation += offset
+    bpy.ops.object.select_all(action="DESELECT")
+
+
+def scene_bbox(single_obj=None, ignore_matrix=False):
+    bbox_min = (math.inf,) * 3
+    bbox_max = (-math.inf,) * 3
+    found = False
+    for obj in scene_meshes() if single_obj is None else [single_obj]:
+        found = True
+        for coord in obj.bound_box:
+            coord = Vector(coord)
+            if not ignore_matrix:
+                coord = obj.matrix_world @ coord
+            bbox_min = tuple(min(x, y) for x, y in zip(bbox_min, coord))
+            bbox_max = tuple(max(x, y) for x, y in zip(bbox_max, coord))
+    if not found:
+        raise RuntimeError("no objects in scene to compute bounding box for")
+    return Vector(bbox_min), Vector(bbox_max)
+
+def scene_meshes():
+    for obj in bpy.context.scene.objects.values():
+        if isinstance(obj.data, (bpy.types.Mesh)):
+            yield obj
+
+
+def randomize_lighting() -> None:
+    light2.energy = random.uniform(300, 600)
+    bpy.data.objects["Area"].location[0] = random.uniform(-1., 1.)
+    bpy.data.objects["Area"].location[1] = random.uniform(-1., 1.)
+    bpy.data.objects["Area"].location[2] = random.uniform(0.5, 1.5)
+
+def set_camera_location(elevation, azimuth, distance):
+    # from https://blender.stackexchange.com/questions/18530/
+    # 用于在 Blender 中随机生成相机的位置，并根据该位置计算相机的旋转，使相机朝向原点（0, 0, 0）
+    x, y, z = sample_spherical(radius_min=1.5, radius_max=2.2, maxz=2.2, minz=-2.2)
+    # print(x, y, z)
+    camera = bpy.data.objects["Camera"]
+    camera.location = x, y, z
+    # print(camera.location)
+    direction = - camera.location
+    rot_quat = direction.to_track_quat('-Z', 'Y')
+    camera.rotation_euler = rot_quat.to_euler()
+    # print(camera.location)
+    return camera
+
+def sample_spherical(radius=3.0, maxz=3.0, minz=0.):
+    correct = False
+    while not correct:
+        vec = np.random.uniform(-1, 1, 3)
+        vec[2] = np.abs(vec[2])
+        vec = vec / np.linalg.norm(vec, axis=0) * radius
+        if maxz > vec[2] > minz:
+            correct = True
+    return vec
+
+def sample_spherical(radius_min=1.5, radius_max=2.0, maxz=1.6, minz=-0.75):
+    correct = False
+    while not correct:
+        vec = np.random.uniform(-1, 1, 3)
+#         vec[2] = np.abs(vec[2])
+        radius = np.random.uniform(radius_min, radius_max, 1)
+        vec = vec / np.linalg.norm(vec, axis=0) * radius[0]
+        if maxz > vec[2] > minz:
+            correct = True
+    return vec
+
+def randomize_camera():
+    elevation = random.uniform(0., 90.)
+    azimuth = random.uniform(0., 360)
+    distance = random.uniform(0.8, 1.6)
+    return set_camera_location(elevation, azimuth, distance)
+
+
+def show_object_in_blender():
+    # 在blender中调用，查看物体与光源等物体
+    # import bpy
+    # import os
+    # filename = "/home/pxn-lyj/Egolee/programs/DiffuRank-liyj/test_blender_scripts.py"
+    # exec(compile(open(filename).read(), filename, 'exec'))
+
+    # from blender_script_zero123 import reset_scene, load_object, normalize_scene, randomize_lighting, randomize_camera
+    object_file = "/home/pxn-lyj/Egolee/data/meshs/obj_000490/taizi1kgxiyiye.obj"
+    # object_file = "/home/pxn-lyj/Egolee/programs/DiffuRank-liyj/example_material/glbs/822202b6b8594342b632b0a39432642a.glb"
+    context = bpy.context
+    scene = context.scene
+    render = scene.render
+
+    cam = scene.objects["Camera"]
+    cam.location = (0, 1.2, 0)
+    cam.data.lens = 35
+    cam.data.sensor_width = 32
+
+    cam_constraint = cam.constraints.new(type="TRACK_TO")
+    cam_constraint.track_axis = "TRACK_NEGATIVE_Z"
+    cam_constraint.up_axis = "UP_Y"
+
+    reset_scene()
+    load_object(object_file)
+    normalize_scene()
+
+    # create an empty object to track
+    # 将之前创建的相机约束的目标设置为刚刚创建的空对象。这样，相机将会跟踪这个空对象的位置, 可以在 3D 视图中看到并操作这个空对象
+    empty = bpy.data.objects.new("Empty", None)
+    scene.collection.objects.link(empty)
+    cam_constraint.target = empty
+
+    randomize_lighting()
+
+    camera = randomize_camera()
+
+    bpy.ops.render.render(write_still=True)    # 需要调用这个才会刷新在世界坐标的位置  matrix_world
+
+    print(camera.location)   # camera.location 是相机对象在其本地坐标系中的位置。它表示相机在其父对象（如果有的话）坐标系中的位置。
+    location, rotation, scale = camera.matrix_world.decompose()[0:3]  # 用途：用于获取相机在全局坐标系中的确切位置。它不受父对象的影响，直接反映了相机在场景中的位置。
+    print(location, rotation, scale)
+    # location 与randomize_camera中设置的位置保持一致
+
+
+def debug_save_rt_zero123_cap3d():
+    # 对比zero123与cap3d的保存相机位姿时的区别
+
+    # get_3x4_RT_matrix_from_blender
+    context = bpy.context
+    scene = context.scene
+    cam = scene.objects["Camera"]
+
+    scale = 0.6
+    for obj in scene_root_objects():
+        obj.scale = obj.scale * scale
+
+    # Apply scale to matrix_world.
+    bpy.context.view_layer.update()
+
+    location, rotation, scale = cam.matrix_world.decompose()[0:3]
+
+    R_world2bcam = rotation.to_matrix().transposed()   # 将c2w转为w2c
+    T_world2bcam = -1*R_world2bcam @ location          # T_w2c = -1 * R_w2c * T_c2w
+    RT = Matrix((
+        R_world2bcam[0][:] + (T_world2bcam[0],),
+        R_world2bcam[1][:] + (T_world2bcam[1],),
+        R_world2bcam[2][:] + (T_world2bcam[2],)
+    ))
+
+    RT_hom = np.eye(4)
+    RT_hom[:3, :] = np.array(RT)
+    opengl_to_colmap = np.array([[1, 0, 0, 0],
+                                 [0, -1, 0, 0],
+                                 [0, 0, -1, 0],
+                                 [0, 0, 0, 1]], dtype=np.float32)
+
+
+    RT_colmap = opengl_to_colmap @ RT_hom
+
+    print(location, rotation, scale)
+
+    matrix = cam.matrix_world
+    origin = list(matrix.col[3])[:3]
+    x_vector = [t/scale.x for t in list(matrix.col[0])[:3]]
+    y_vector = [t/scale.y for t in list(-matrix.col[1])[:3]]
+    z_vector = [t/scale.z for t in list(-matrix.col[2])[:3]]
+    rotation_matrix = np.array([x_vector, y_vector, z_vector]).T   # c2w  这里的转置是为了将行向量转为列向量
+    translation_vector = np.array(origin)
+
+    rt_matrix = np.eye(4)
+    rt_matrix[:3, :3] = rotation_matrix
+    rt_matrix[:3, 3] = translation_vector
+    w2c = np.linalg.inv(rt_matrix)
+
+    print(np.allclose(w2c, RT_colmap))
+    print(w2c-RT_colmap)
+    print("ff")
+
+
 if __name__ == "__main__":
     print("Start")
-    # read_img()
+    # read_render_img()
     # debug_load_obj()
-    UNIFORM_LIGHT_DIRECTION = [0.09387503, -0.63953443, -0.7630093]
-    debug_create_uniform_light()
+    # debug_create_uniform_light()
+    # load_depth()
+    # debug_camera_matrix_world_rot_matrix()
+    # debug_camera_matrix_world_scale()
+    # show_object_in_blender()
+    debug_save_rt_zero123_cap3d()
     print("End")
 
 print("ffff")
